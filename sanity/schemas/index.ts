2a0089628d1fb3807e37294fef4f8d1ec6855b3e
Francis Lagares <francis.lagares@gmail.com>
2023-12-10 20:36:35 +0100

    feat: define heroImages schema

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,4 +1,5 @@
 import category from './category'
+import heroImages from './heroImages'
 import product from './product'
 
-export const schemaTypes = [product, category]
+export const schemaTypes = [product, category, heroImages]
