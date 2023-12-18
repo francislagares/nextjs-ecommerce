cb07b89694bf2bd4f58d341fa022e34c13130b8a
Francis Lagares <francis.lagares@gmail.com>
2023-12-18 21:26:10 +0100

    feat: create Newest products component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from './components/Hero';
+import Newest from './components/Newest';
 
 const Home = () => {
   return (
     <div className='bg-white pb-6 sm:pb-8 lg:pb-12'>
       <Hero />
+      <Newest />
     </div>
   );
 };
