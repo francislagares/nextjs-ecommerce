bed76f6c6842ac2fb789df9c5e752ea254dcfd67
Francis Lagares <francis.lagares@gmail.com>
2023-12-25 20:13:27 +0100

    feat: implement ShoppingCartModal component

--- src/app/components/ShoppingCartModal.tsx
+++ src/app/components/ShoppingCartModal.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import Image from 'next/image';
 import { useShoppingCart } from 'use-shopping-cart';
 
+import { Button } from '@/components/ui/button';
 import {
   Sheet,
   SheetContent,
@@ -10,7 +12,14 @@ import {
 } from '@/components/ui/sheet';
 
 const ShoppingCartModal = () => {
-  const { cartCount, shouldDisplayCart, handleCartClick } = useShoppingCart();
+  const {
+    cartCount,
+    shouldDisplayCart,
+    handleCartClick,
+    cartDetails,
+    removeItem,
+    totalPrice,
+  } = useShoppingCart();
 
   return (
     <Sheet open={shouldDisplayCart} onOpenChange={() => handleCartClick()}>
@@ -24,9 +33,74 @@ const ShoppingCartModal = () => {
               {cartCount === 0 ? (
                 <h1 className='py-6'>You dont have any items</h1>
               ) : (
-                <h1>Hey you have some items.</h1>
+                <>
+                  {Object.values(cartDetails ?? {}).map(entry => (
+                    <li key={entry.id} className='flex py-6'>
+                      <div className='h-24 w-24 shrink-0 overflow-hidden rounded-md border border-gray-200'>
+                        <Image
+                          src={entry.image as string}
+                          alt='Product image'
+                          width={100}
+                          height={100}
+                        />
+                      </div>
+
+                      <div className='ml-4 flex flex-1 flex-col'>
+                        <div>
+                          <div className='flex justify-between text-base font-medium text-gray-900'>
+                            <h3>{entry.name}</h3>
+                            <p className='ml-4'>${entry.price}</p>
+                          </div>
+                          <p className='mt-1 line-clamp-2 text-sm text-gray-500'>
+                            {entry.description}
+                          </p>
+                        </div>
+
+                        <div className='flex flex-1 items-end justify-between text-sm'>
+                          <p className='text-gray-500'>QTY: {entry.quantity}</p>
+
+                          <div className='flex'>
+                            <button
+                              type='button'
+                              onClick={() => removeItem(entry.id)}
+                              className='font-medium text-primary hover:text-primary/80'
+                            >
+                              Remove
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </>
               )}
             </ul>
+          </div>
+
+          <div className='border-t border-gray-200 px-4 py-6 sm:px-6'>
+            <div className='flex justify-between text-base font-medium text-gray-900'>
+              <p>Subtotal:</p>
+              <p>${totalPrice}</p>
+            </div>
+            <p className='mt-0.5 text-sm text-gray-500'>
+              Shipping and taxes are calculated at checkout.
+            </p>
+
+            <div className='mt-6'>
+              <Button className='w-full'>Checkout</Button>
+            </div>
+
+            <div className='mt-6 flex justify-center text-center text-sm text-gray-500'>
+              <p>
+                OR{' '}
+                <button
+                  onClick={() => handleCartClick()}
+                  className=' font-medium text-primary hover:text-primary/80'
+                >
+                  Continue Shopping
+                </button>
+              </p>
+            </div>
           </div>
         </div>
       </SheetContent>
